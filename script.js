e1bea8f35b9b6219a84bbdcc32226aee75faffa8
Grace Lu <2018glu@tjhsst.edu>
2016-07-23 13:07:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+var main = function(){
     $("#name").click(function(){
       $(this).effect('bounce', {times: 2});
       
     })
-});
+};
 $(document).ready(main);
